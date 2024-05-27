54af34cf7b062766124e63e32e04590cf64daf7b
Kris-tina30 <kristina.pichkar.15@gmail.com>
2024-05-27 23:02:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './utils/db/initMongoConnection.js';
-import { Contact } from './utils/db/models/contacts.js';
 
 (async () => {
   await initMongoConnection();
-  const contacts = await Contact.find({});
-  console.log(contacts);
+
   setupServer();
 })();
